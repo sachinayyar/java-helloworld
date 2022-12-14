ffdb535fd8523d7877730083e9570a7d6f02d335
Sachin Ayyar <91127350+sachinayyar@users.noreply.github.com>
2022-12-14 11:21:53 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,10 +4,7 @@ import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
-      LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime);
-
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+      
+	    System.out.println("sachin");
     }
 }
